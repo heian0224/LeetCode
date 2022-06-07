54408be2c3aec88adfe5e6c1b9db4920080f13fb
Cong <heian0224@gmail.com>
2022-06-07 23:48:39 +0800

    add problem1 solution.
    Signed-off-by: Cong <heian0224@gmail.com>

---
+++ go/go.mod
@@ -0,0 +1,3 @@
+module com.liucongblog.leetcode
+
+go 1.16
--- java/com/liucongblog/leetcode/Problem1.java
+++ java/com/liucongblog/leetcode/Problem1.java
@@ -1,8 +1,18 @@
 package com.liucongblog.leetcode;
 
+import java.util.HashMap;
+
 public class Problem1 {
     public int[] twoSum(int[] nums, int target) {
-
+        HashMap<Integer, Integer> searchMap=new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int another = target- nums[i];
+            if(searchMap.containsKey(another)){
+                return  new int[]{i,searchMap.get(another)};
+            }else {
+                searchMap.put(nums[i],i);
+            }
+        }
         return null;
     }
 }
