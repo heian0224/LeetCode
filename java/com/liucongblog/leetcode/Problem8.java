9d6029890deffdc42e6ba58710d8b81f7e5e8f6a
Cong <heian0224@gmail.com>
2022-07-07 23:55:37 +0800

    add leetcode problem 8 solution.
    Signed-off-by: Cong <heian0224@gmail.com>

---
+++ java/com/liucongblog/leetcode/Problem8.java
@@ -0,0 +1,29 @@
+package com.liucongblog.leetcode;
+
+public class Problem8 {
+    public int myAtoi(String s) {
+        if(s==null ){
+            return 0;
+        }
+        int sign =1;
+        int res =0;
+        String trimmedStr=s.trim();
+        String readStr=trimmedStr;
+        if(trimmedStr.isEmpty()){
+            return 0;
+        }
+        if(trimmedStr.charAt(0)=='+'||trimmedStr.charAt(0)=='+'){
+
+        }
+        StringBuilder integerStr=new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+
+
+
+        }
+
+
+        return  sign* res;
+
+    }
+}
