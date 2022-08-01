4332b1dbacd360028da56bf86a56a88974bb4516
Cong <heian0224@gmail.com>
2022-08-02 00:13:35 +0800

    add  problem 33 class. (2022-08-01)
    Signed-off-by: Cong <heian0224@gmail.com>

--- java/com/liucongblog/leetcode/Problem33.java
+++ java/com/liucongblog/leetcode/Problem33.java
@@ -47,54 +47,65 @@ public class Problem33 {
      * @param target
      * @return
      */
-    public int search(int[] nums, int target) {
+    public static int search(int[] nums, int target) {
 
         if (nums == null) {
             return -1;
         }
-        if (nums.length==1) {
-            return nums[0]==target? 0:-1;
+        if (nums.length == 1) {
+            return nums[0] == target ? 0 : -1;
         }
-        int leftIndex=0;
-        int rightIndex=nums.length-1;
-        while (leftIndex<rightIndex+1){
-            int leftNum=nums[leftIndex];
+        int leftIndex = 0;
+        int rightIndex = nums.length - 1;
+        if (target == nums[leftIndex]) {
+            return leftIndex;
+        }
+        if (target == nums[rightIndex]) {
+            return rightIndex;
+        }
+
+        while (leftIndex + 1 < rightIndex) {
+            int leftNum = nums[leftIndex];
             int rightNum = nums[rightIndex];
-            int middleIndex= (leftIndex+rightIndex)/2;
+            int middleIndex = (leftIndex + rightIndex) / 2;
             int middleNum = nums[middleIndex];
-            if(target==leftNum){
+            if (target == leftNum) {
                 return leftIndex;
             }
-            if(target==rightNum){
+            if (target == rightNum) {
                 return rightIndex;
             }
-            if(target==middleNum){
+            if (target == middleNum) {
                 return middleIndex;
             }
-            if(target<leftNum && target>rightNum){
+            if (target < leftNum && target > rightNum) {
                 return -1;
             }
-            if(middleNum>leftNum){
+            if (middleNum > leftNum) {
                 //rotate index in right area
-                if(target>middleNum||target<rightNum){
-                    leftIndex=middleIndex;
-
-                }else {
-                    rightIndex=middleIndex;
+                if (target > leftNum && target < middleNum) {
+                    rightIndex = middleIndex;
+                } else {
+                    leftIndex = middleIndex;
                 }
 
-            }else {
+            } else {
                 //rotate index in left area
-                if(target){
-
+                if (target < rightNum && target > middleNum) {
+                    leftIndex = middleIndex;
+                } else {
+                    rightIndex = middleIndex;
                 }
             }
 
         }
 
 
-        
         return -1;
 
+    }
+
+    public static void main(String[] args) {
+        System.out.println(search(new int[]{2, 3, 4, 5, 6, 7, 8, 9, 1}, 3));
     }
 }
