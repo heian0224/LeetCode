560be3fb18a234b388d095b34c2e326708768112
Cong <heian0224@gmail.com>
2022-08-03 01:15:14 +0800

    add  problem 34 class. (2022-08-02)
    Signed-off-by: Cong <heian0224@gmail.com>

--- java/com/liucongblog/leetcode/Problem34.java
+++ java/com/liucongblog/leetcode/Problem34.java
@@ -1,5 +1,7 @@
 package com.liucongblog.leetcode;
 
+import java.util.Arrays;
+
 /**
  * <p>Project: LeetCode</p>
  * <p>File: com.liucongblog.leetcode.Problem34</p>
@@ -44,16 +46,107 @@ public class Problem34 {
      * @param target target value.
      * @return the starting and ending position of a given target value. If target is not found in the array, return [-1, -1].
      */
-    public int[] searchRange(int[] nums, int target) {
-        if (nums == null) {
-            return new int[]{-1,-1};
+    public static int[] searchRange(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return new int[]{-1, -1};
         }
-        if(target<nums[0]|| target>nums[nums.length-1]){
-            return new int[]{-1,-1};
+        if (target < nums[0] || target > nums[nums.length - 1]) {
+            return new int[]{-1, -1};
         }
+        if (target == nums[0] && target == nums[nums.length - 1]) {
+            return new int[]{0, nums.length - 1};
+        }
+        if (nums.length == 2) {
+            if (nums[0] == target && nums[1] != target) {
+                return new int[]{0, 0};
+            }
+            if (nums[0] == target && nums[1] == target) {
+                return new int[]{0, 1};
+            }
+            if (nums[0] != target && nums[1] == target) {
+                return new int[]{1, 1};
+            }
+        }
+        int foundedIndex = -1;
+        int leftIndex = 0;
+        int rightIndex = nums.length - 1;
+        if (nums[leftIndex] == target || nums[rightIndex] == target) {
+            foundedIndex = nums[leftIndex] == target ? leftIndex : rightIndex;
+        } else {
+            while (leftIndex + 1 < rightIndex) {
+                int middleIndex = (leftIndex + rightIndex) / 2;
+                int middleNum = nums[middleIndex];
+                if (target == middleNum) {
+                    foundedIndex = middleIndex;
+                    break;
+                }
+                if (middleNum < target) {
+                    leftIndex = middleIndex;
+                } else {
+                    rightIndex = middleIndex;
+                }
+            }
+        }
+        if (foundedIndex == -1) {
+            return new int[]{-1, -1};
+        }
+        int resStartIndex = foundedIndex;
+        int resEndIndex = foundedIndex;
+        if (foundedIndex == leftIndex) {
+            resStartIndex = leftIndex;
+        } else {
+            int startIndex = leftIndex;
+            int endIndex = foundedIndex;
+            if (endIndex == startIndex + 1) {
+                resStartIndex = endIndex;
+            } else {
+                // find first one that equal target and previous one less than target.
+                while (startIndex + 1 < endIndex) {
+                    int middleIndex = (startIndex + endIndex) / 2;
+                    int middleNum = nums[middleIndex];
 
+                    if (target == middleNum && (middleIndex == 0 || nums[middleIndex - 1] < target)) {
+                        resStartIndex = middleIndex;
+                        break;
+                    }
+                    if (middleNum < target) {
+                        startIndex = middleIndex;
+                    } else {
+                        endIndex = middleIndex;
+                    }
+                }
+            }
 
+        }
+        if (foundedIndex == rightIndex) {
+            resEndIndex = rightIndex;
+        } else {
+            int startIndex = foundedIndex;
+            int endIndex = rightIndex;
+            if (endIndex == startIndex + 1) {
+                resEndIndex = startIndex;
+            } else {
+                // find first one that equal target and follow one greater than target.
+                while (startIndex + 1 < endIndex) {
+                    int middleIndex = (startIndex + endIndex) / 2;
+                    int middleNum = nums[middleIndex];
 
-        return new int[]{1};
+                    if (target == middleNum && (middleIndex == nums.length - 1 || nums[middleIndex + 1] > target)) {
+                        resEndIndex = middleIndex;
+                        break;
+                    }
+                    if (middleNum > target) {
+                        endIndex = middleIndex;
+                    } else {
+                        startIndex = middleIndex;
+                    }
+                }
+            }
+        }
+        return new int[]{resStartIndex, resEndIndex};
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(searchRange(new int[]{3, 3, 3}, 3)));
     }
 }
