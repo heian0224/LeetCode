f026a8197826b48dddc55515e7efd02971f62a85
Cong <heian0224@gmail.com>
2022-08-11 23:26:08 +0800

    add  problem 44 class. (  for 2022-08-11)
    Signed-off-by: Cong <heian0224@gmail.com>

---
+++ java/com/liucongblog/leetcode/Problem44.java
@@ -0,0 +1,49 @@
+package com.liucongblog.leetcode;
+
+/**
+ * <p>Project: LeetCode</p>
+ * <p>File: com.liucongblog.leetcode.Problem44</p>
+ * <p>Description: </p>
+ *
+ * @author Cong
+ * @date 2022/8/11
+ */
+public class Problem44 {
+    /**
+     * 44. Wildcard Matching
+     * <p>
+     * Given an input string (s) and a pattern (p), implement wildcard pattern matching with support for '?' and '*' where:
+     * <p>
+     * '?' Matches any single character.
+     * '*' Matches any sequence of characters (including the empty sequence).
+     * The matching should cover the entire input string (not partial).
+     * <p>
+     * <p>
+     * <p>
+     * Example 1:
+     * <p>
+     * Input: s = "aa", p = "a"
+     * Output: false
+     * Explanation: "a" does not match the entire string "aa".
+     * Example 2:
+     * <p>
+     * Input: s = "aa", p = "*"
+     * Output: true
+     * Explanation: '*' matches any sequence.
+     * Example 3:
+     * <p>
+     * Input: s = "cb", p = "?a"
+     * Output: false
+     * Explanation: '?' matches 'c', but the second letter is 'a', which does not match 'b'.
+     *
+     * @param s to be validate string
+     * @param p string pattern
+     * @return match or not
+     */
+    public boolean isMatch(String s, String p) {
+
+
+        return false;
+    }
+
+}
