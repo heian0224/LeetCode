728f11206b9101e11134d475eeabb3fae4d6791e
Cong <heian0224@gmail.com>
2022-08-14 11:17:45 +0800

    add  problem 48 class. (for 2022-08-14)
    Signed-off-by: Cong <heian0224@gmail.com>

---
+++ java/com/liucongblog/leetcode/Problem47.java
@@ -0,0 +1,45 @@
+package com.liucongblog.leetcode;
+
+import java.util.List;
+
+/**
+ * <p>Project: LeetCode</p>
+ * <p>File: com.liucongblog.leetcode.Problem47</p>
+ * <p>Description: </p>
+ *
+ * @author Cong
+ * @date 2022/8/13
+ */
+public class Problem47 {
+    /**
+     * 47. Permutations II
+     * <p>
+     * Given a collection of numbers, nums, that might contain duplicates, return all possible unique permutations in any order.
+     * <p>
+     * <p>
+     * <p>
+     * Example 1:
+     * <p>
+     * Input: nums = [1,1,2]
+     * Output:
+     * [[1,1,2],
+     * [1,2,1],
+     * [2,1,1]]
+     * Example 2:
+     * <p>
+     * Input: nums = [1,2,3]
+     * Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+     * <p>
+     * <p>
+     * Constraints:
+     * <p>
+     * 1 <= nums.length <= 8
+     * -10 <= nums[i] <= 10
+     *
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        return null;
+    }
+}
