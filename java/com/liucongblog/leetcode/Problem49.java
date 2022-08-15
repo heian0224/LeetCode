4b0b9d6e21c0396259fb6a468cb8c6f3264fe05e
Cong <heian0224@gmail.com>
2022-08-15 23:47:04 +0800

    add  problem 49 class. (re-add for 2022-08-15)
    Signed-off-by: Cong <heian0224@gmail.com>

---
+++ java/com/liucongblog/leetcode/Problem49.java
@@ -0,0 +1,52 @@
+package com.liucongblog.leetcode;
+
+import java.util.List;
+
+/**
+ * <p>Project: LeetCode</p>
+ * <p>File: com.liucongblog.leetcode.Problem49</p>
+ * <p>Description: </p>
+ *
+ * @author Cong
+ * @date 2022/8/15
+ */
+public class Problem49 {
+    /**
+     * 49. Group Anagrams
+     * <p>
+     * Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+     * <p>
+     * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+     * <p>
+     * <p>
+     * <p>
+     * Example 1:
+     * <p>
+     * Input: strs = ["eat","tea","tan","ate","nat","bat"]
+     * Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+     * Example 2:
+     * <p>
+     * Input: strs = [""]
+     * Output: [[""]]
+     * Example 3:
+     * <p>
+     * Input: strs = ["a"]
+     * Output: [["a"]]
+     * <p>
+     * <p>
+     * Constraints:
+     * <p>
+     * 1 <= strs.length <= 104
+     * 0 <= strs[i].length <= 100
+     * strs[i] consists of lowercase English letters.
+     *
+     * @param strs
+     * @return
+     */
+    public List<List<String>> groupAnagrams(String[] strs) {
+        strs[0]
+
+
+        return null;
+    }
+}
