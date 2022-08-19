f510b4aa76e969833991d9056834de6e80f65a24
Cong <heian0224@gmail.com>
2022-08-19 22:43:06 +0800

    add  problem 53 class. (for 2022-08-19)
    Signed-off-by: Cong <heian0224@gmail.com>

---
+++ java/com/liucongblog/leetcode/Problem53.java
@@ -0,0 +1,52 @@
+package com.liucongblog.leetcode;
+
+/**
+ * <p>Project: LeetCode</p>
+ * <p>File: com.liucongblog.leetcode.Problem53</p>
+ * <p>Description: </p>
+ *
+ * @author Cong
+ * @date 2022/8/18
+ */
+public class Problem53 {
+
+    /**
+     * 53. Maximum Subarray
+     * <p>
+     * Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+     * <p>
+     * A subarray is a contiguous part of an array.
+     * <p>
+     * <p>
+     * <p>
+     * Example 1:
+     * <p>
+     * Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+     * Output: 6
+     * Explanation: [4,-1,2,1] has the largest sum = 6.
+     * Example 2:
+     * <p>
+     * Input: nums = [1]
+     * Output: 1
+     * Example 3:
+     * <p>
+     * Input: nums = [5,4,-1,7,8]
+     * Output: 23
+     * <p>
+     * <p>
+     * Constraints:
+     * <p>
+     * 1 <= nums.length <= 105
+     * -104 <= nums[i] <= 104
+     * <p>
+     * <p>
+     * Follow up: If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+     *
+     * @param nums
+     * @return
+     */
+    public int maxSubArray(int[] nums) {
+        return -1;
+    }
+
+}
