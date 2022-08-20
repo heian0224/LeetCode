df0ccf22e3df2fb6f5803eba72ef1efb9bd823a0
Cong <heian0224@gmail.com>
2022-08-20 23:04:50 +0800

    add  problem 54 class. (for 2022-08-20)
    Signed-off-by: Cong <heian0224@gmail.com>

---
+++ java/com/liucongblog/leetcode/Problem54.java
@@ -0,0 +1,47 @@
+package com.liucongblog.leetcode;
+
+import java.util.List;
+
+/**
+ * <p>Project: LeetCode</p>
+ * <p>File: com.liucongblog.leetcode.Problem54</p>
+ * <p>Description: </p>
+ *
+ * @author Cong
+ * @date 2022/8/20
+ */
+public class Problem54 {
+    /**
+     * 54. Spiral Matrix
+     * <p>
+     * Given an m x n matrix, return all elements of the matrix in spiral order.
+     * <p>
+     * <p>
+     * <p>
+     * Example 1:
+     * <p>
+     * <p>
+     * Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+     * Output: [1,2,3,6,9,8,7,4,5]
+     * Example 2:
+     * <p>
+     * <p>
+     * Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+     * Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+     * <p>
+     * <p>
+     * Constraints:
+     * <p>
+     * m == matrix.length
+     * n == matrix[i].length
+     * 1 <= m, n <= 10
+     * -100 <= matrix[i][j] <= 100
+     *
+     * @param matrix
+     * @return
+     */
+    public List<Integer> spiralOrder(int[][] matrix) {
+        return null;
+    }
+
+}
