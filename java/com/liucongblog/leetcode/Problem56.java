4afa07fd014b1836c66752221bea47098f93f9b9
Cong <heian0224@gmail.com>
2022-08-23 23:53:23 +0800

    add  problem 56 class. (for 2022-08-23)
    Signed-off-by: Cong <heian0224@gmail.com>

--- java/com/liucongblog/leetcode/Problem56.java
+++ java/com/liucongblog/leetcode/Problem56.java
@@ -1,5 +1,10 @@
 package com.liucongblog.leetcode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 /**
  * <p>Project: LeetCode</p>
  * <p>File: com.liucongblog.leetcode.Problem56</p>
@@ -38,7 +43,49 @@ public class Problem56 {
      * @param intervals
      * @return
      */
-    public int[][] merge(int[][] intervals) {
-        return null;
+    public static int[][] merge(int[][] intervals) {
+        int n = intervals.length;
+        Arrays.sort(intervals, Comparator.comparingInt(i -> i[0]));
+
+
+        List<Integer> resStartList = new ArrayList<>();
+        List<Integer> resEndList = new ArrayList<>();
+        List<Integer> addedIndexList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (addedIndexList.contains(i)) {
+                continue;
+            }
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+            int maxEnd = end;
+            for (int j = 0; j < n; j++) {
+                if (j == i) {
+                    continue;
+                }
+                int currentStart = intervals[j][0];
+                int currentEnd = intervals[j][1];
+                if (maxEnd >= currentStart) {
+                    addedIndexList.add(j);
+                    if (currentEnd > maxEnd) {
+                        maxEnd = currentEnd;
+                    }
+                }
+
+
+            }
+            resStartList.add(start);
+            resEndList.add(maxEnd);
+            addedIndexList.add(i);
+        }
+        int[][] res = new int[resStartList.size()][2];
+        for (int i = 0; i < resStartList.size(); i++) {
+            res[i][0] = resStartList.get(i);
+            res[i][1] = resEndList.get(i);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.deepToString(merge(new int[][]{{1, 4}, {0,2}, {3,5} })));
     }
 }
