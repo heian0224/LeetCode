6bdc6180c9c54c30d1a8b826fd63c61c6ed94870
Cong <heian0224@gmail.com>
2022-08-23 22:40:16 +0800

    add  problem 56 class. (for 2022-08-23)
    Signed-off-by: Cong <heian0224@gmail.com>

---
+++ java/com/liucongblog/leetcode/Problem56.java
@@ -0,0 +1,44 @@
+package com.liucongblog.leetcode;
+
+/**
+ * <p>Project: LeetCode</p>
+ * <p>File: com.liucongblog.leetcode.Problem56</p>
+ * <p>Description: </p>
+ *
+ * @author Cong
+ * @date 2022/8/22
+ */
+public class Problem56 {
+
+    /**
+     * 56. Merge Intervals
+     * <p>
+     * Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+     * <p>
+     * <p>
+     * <p>
+     * Example 1:
+     * <p>
+     * Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+     * Output: [[1,6],[8,10],[15,18]]
+     * Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
+     * Example 2:
+     * <p>
+     * Input: intervals = [[1,4],[4,5]]
+     * Output: [[1,5]]
+     * Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+     * <p>
+     * <p>
+     * Constraints:
+     * <p>
+     * 1 <= intervals.length <= 104
+     * intervals[i].length == 2
+     * 0 <= starti <= endi <= 104
+     *
+     * @param intervals
+     * @return
+     */
+    public int[][] merge(int[][] intervals) {
+        return null;
+    }
+}
