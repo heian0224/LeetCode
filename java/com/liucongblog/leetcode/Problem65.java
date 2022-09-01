93eda9dd309d5d05c3330732764e59127c42f483
Cong <heian0224@gmail.com>
2022-09-01 23:15:43 +0800

    add  problem 65 class. (for 2022-09-01)
    Signed-off-by: Cong <heian0224@gmail.com>

---
+++ java/com/liucongblog/leetcode/Problem65.java
@@ -0,0 +1,62 @@
+package com.liucongblog.leetcode;
+
+/**
+ * <p>Project: LeetCode</p>
+ * <p>File: com.liucongblog.leetcode.Problem65</p>
+ * <p>Description: </p>
+ *
+ * @author Cong
+ * @date 2022/8/31
+ */
+public class Problem65 {
+    /**
+     * 65. Valid Number
+     * A valid number can be split up into these components (in order):
+     * <p>
+     * A decimal number or an integer.
+     * (Optional) An 'e' or 'E', followed by an integer.
+     * A decimal number can be split up into these components (in order):
+     * <p>
+     * (Optional) A sign character (either '+' or '-').
+     * One of the following formats:
+     * One or more digits, followed by a dot '.'.
+     * One or more digits, followed by a dot '.', followed by one or more digits.
+     * A dot '.', followed by one or more digits.
+     * An integer can be split up into these components (in order):
+     * <p>
+     * (Optional) A sign character (either '+' or '-').
+     * One or more digits.
+     * For example, all the following are valid numbers: ["2", "0089", "-0.1", "+3.14", "4.", "-.9", "2e10", "-90E3", "3e+7", "+6e-1", "53.5e93", "-123.456e789"], while the following are not valid numbers: ["abc", "1a", "1e", "e3", "99e2.5", "--6", "-+3", "95a54e53"].
+     * <p>
+     * Given a string s, return true if s is a valid number.
+     * <p>
+     * <p>
+     * <p>
+     * Example 1:
+     * <p>
+     * Input: s = "0"
+     * Output: true
+     * Example 2:
+     * <p>
+     * Input: s = "e"
+     * Output: false
+     * Example 3:
+     * <p>
+     * Input: s = "."
+     * Output: false
+     * <p>
+     * <p>
+     * Constraints:
+     * <p>
+     * 1 <= s.length <= 20
+     * s consists of only English letters (both uppercase and lowercase), digits (0-9), plus '+', minus '-', or dot '.'.
+     *
+     * @param s
+     * @return
+     */
+
+    public boolean isNumber(String s) {
+        return false;
+
+    }
+}
